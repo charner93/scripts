058116a298890b75f615796c7847f35bfeb05067
Charles Harner <charner93@gmail.com>
2018-01-22 07:29:47 -0600

    Update JFP.js

--- JFP.js
+++ JFP.js
@@ -1,4 +1,7 @@
-getForm = document.getElementByTagName("form");
-usernameForm = getForm[0][0].value
-passwordForm = getForm[0][0].value
-alert(usernameForm + " " + passwordForm)
+function onFormSubmit{
+	getForm = document.getElementByTagName("form");
+	usernameForm = getForm[0][0].value;
+	passwordForm = getForm[0][0].value;
+	alert(usernameForm + " " + passwordForm);
+}
+document.forms[0].onsubmit = onFormSubmit;
